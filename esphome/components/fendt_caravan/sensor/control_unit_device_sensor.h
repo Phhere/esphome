b155cd0e754f51641c86ae504c10797a21ee6783
Philipp Helo Rehs <Philipp+github@rehs.me>
2026-08-17 22:33:05 +0200

    Revert "report all entities always"
    This reverts commit f37ef135b61757fc09fc7d870a8b8ec0b8eb46fe.

--- esphome/components/fendt_caravan/sensor/control_unit_device_sensor.h
+++ esphome/components/fendt_caravan/sensor/control_unit_device_sensor.h
@@ -7,7 +7,6 @@
 #include "esphome/components/fendt_caravan/variable.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/switch/switch.h"
-#include "esphome/core/application.h"
 #include "esphome/core/string_ref.h"
 #include "esphome/core/component.h"
 #include "esphome/core/log.h"
@@ -22,7 +21,6 @@ class ControlUnitDeviceSensor : public CaravanDeviceComponent, public sensor::Se
   void setup() override;
   void dump_config() override;
   void on_state_change_command(const std::string &tag, const std::string &command) override;
-  void set_auto_report_entities(bool enabled) { this->auto_report_entities_ = enabled; }
   SUB_SWITCH(main_switch);
   SUB_SENSOR(temp_in);
   SUB_SENSOR(temp_out);
@@ -55,31 +53,6 @@ class ControlUnitDeviceSensor : public CaravanDeviceComponent, public sensor::Se
 
  protected:
   void on_data_decoded(IVariable *variable) override;
-  template<typename T>
-  void add_auto_report_sensor_(Variable<T> *variable) {
-    auto *entity = new sensor::Sensor();
-    entity->set_name(variable->get_name());
-    entity->set_unique_id("fendt_caravan_" + variable->get_name());
-    App.register_component(entity);
-    variable->set_on_decode_callback([entity](const T &value) { entity->publish_state(value); });
-  }
-  void add_auto_report_switch_(Variable<bool> *variable) {
-    auto *entity = new switch::Switch();
-    entity->set_name(variable->get_name());
-    entity->set_unique_id("fendt_caravan_" + variable->get_name());
-    App.register_component(entity);
-    variable->set_on_decode_callback([entity](const bool &value) { entity->publish_state(value); });
-  }
-  void add_auto_report_text_sensor_(Variable<std::string> *variable) {
-    auto *entity = new text_sensor::TextSensor();
-    entity->set_name(variable->get_name());
-    entity->set_unique_id("fendt_caravan_" + variable->get_name());
-    App.register_component(entity);
-    variable->set_on_decode_callback([entity](const std::string &value) { entity->publish_state(value); });
-  }
-  void add_auto_report_entities_();
-
-  bool auto_report_entities_{true};
 };
 }  // namespace esphome::fendt_caravan
 #endif
